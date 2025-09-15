a935d68539b65f3d6f734a0b68ea44425f0fd9c4
Pravat Patra <pravatpatra1997@gmail.com>
2025-09-16 00:03:19 +0530

    API commit D1.01

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,HTTPException,Path
+from fastapi import FastAPI,HTTPException,Path,Query
 import json
 
 app=FastAPI()
@@ -25,4 +25,26 @@ def fetch_patient_data(patients_id:str=Path(...,description="enter the patient I
         return data[patients_id]
     
     raise HTTPException(status_code=404,detail="value not found")
+
+#create a query parameter
+@app.get("/sort")
+def sort_details(sort_by : str = Query(...,description="tell us in based on which you want to sort. "),order:str =Query(description="Tell us how you want to see the data means in asc or desc")):
+
+    sort_params=['height','weight','bmi']
+    order_params=['asc','desc']
+
+    if sort_by not in sort_params:
+        raise HTTPException(status_code=400,detail=f'Invaid entry. Value must be from {sort_params}')
+    
+    if order not in order_params:
+        raise HTTPException(status_code=400,detail=f'Invaid entry. Value must be from {order_params}')
+    
+    data = load_data()
+
+    sort_order = True if order=='desc' else False
+
+    sorted_data = sorted(data.values(), key=lambda x: x.get(sort_by, 0), reverse=sort_order)
+
+    return sorted_data
+    
 
